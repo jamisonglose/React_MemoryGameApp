c70bcb4462182f3e648ddefb31ef23936ba7bb8f
Jamison Glose <jamisonglose@yahoo.com>
2025-02-13 21:47:53 -0600

    Setting data in state

--- MemoryGame/src/App.jsx
+++ MemoryGame/src/App.jsx
@@ -1,40 +1,45 @@
-import { useState } from 'react'
-import Form from './components/Form'
-import MemoryCard from './components/MemoryCard'
+import { useState } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
 
 export default function App() {
-    const [isGameOn, setIsGameOn] = useState(false)
-    
-    async function startGame(e) {
-        e.preventDefault()
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
 
-        try {
-           const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature");
+  console.log(emojisData);
 
-           if(!response.ok){
+  async function startGame(e) {
+    e.preventDefault();
 
-           }
-           
-           const data = await response.json();
-           console.log(data);
-           setIsGameOn(true)
-            
-        } catch (error) {
-            throw new Error("Could not fetch data from API")
-        }
+    try {
+      const response = await fetch(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
 
-        
+      if (!response.ok) {
+        throw new Error("Could not fetch data from API");
+      }
+
+      const data = await response.json();
+      const dataSample = data.slice(0, 5);
+
+      setEmojisData(dataSample);
+
+      setIsGameOn(true);
+    } catch (err) {
+      console.error(err);
     }
-    
-    function turnCard() {
-        console.log("Memory card clicked")
-    }
-    
-    return (
-        <main>
-            <h1>Memory</h1>
-            {!isGameOn && <Form handleSubmit={startGame} />}
-            {isGameOn && <MemoryCard handleClick={turnCard} />}
-        </main>
-    )
+  }
+
+  function turnCard() {
+    console.log("Memory card clicked");
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} />}
+    </main>
+  );
 }
