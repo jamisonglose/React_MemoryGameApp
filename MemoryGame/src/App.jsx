a68f00c6ea533899e50374d355c8eb0486697cc9
Jamison Glose <jamisonglose@yahoo.com>
2025-02-14 21:40:22 -0600

    Finished getting random emoji cards and duplicating. Modifed onClick function in MemoryCard to pass name and index of object.

--- MemoryGame/src/App.jsx
+++ MemoryGame/src/App.jsx
@@ -5,7 +5,8 @@ import MemoryCard from "./components/MemoryCard";
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
   const [emojisData, setEmojisData] = useState([]);
-  
+  const [selectedCards, setSelectedCards] = useState([]);
+
   async function startGame(e) {
     e.preventDefault();
 
@@ -19,9 +20,10 @@ export default function App() {
       }
 
       const data = await response.json();
-      const dataSample = data.slice(0, 5);
+      const dataSlice = await getDataSlice(data);
+      const emojisArray = await getEmojisArray(dataSlice);
 
-      setEmojisData(dataSample);
+      setEmojisData(emojisArray);
 
       setIsGameOn(true);
     } catch (err) {
@@ -29,8 +31,38 @@ export default function App() {
     }
   }
 
-  function turnCard() {
-    console.log("Memory card clicked");
+  async function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+    return randomIndices.map((index) => data[index]);
+  }
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+    return randomIndicesArray;
+  }
+
+  async function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+    return pairedEmojisArray;
+  }
+
+  function turnCard(index, name) {
+    setSelectedCards([{ name, index }]);    
+    console.log(selectedCards);
   }
 
   return (
--- MemoryGame/src/components/MemoryCard.jsx
+++ MemoryGame/src/components/MemoryCard.jsx
@@ -3,7 +3,10 @@ import { decodeEntity } from "html-entities";
 export default function MemoryCard({ handleClick, data }) {
   const emojiEl = data.map((emoji, index) => (
     <li key={index} className="card-item">
-      <button className="btn btn--emoji" onClick={handleClick}>
+      <button
+        className="btn btn--emoji"
+        onClick={() => handleClick(index, emoji.name)}
+      >
         {decodeEntity(emoji.htmlCode[0])}
       </button>
     </li>
